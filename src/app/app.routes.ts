b1dfec21d8df92648e06911ef6def5f047fe5cf5
Kevin Wiegand <KevinAllenWiegand@users.noreply.github.com>
2025-03-04 13:16:18 -0500

    Add Navigation, do some styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ItemSearchComponent } from './home/item-search.component';
+import { MyItemsComponent } from './home/my-items.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'itemsearch', component: ItemSearchComponent },
+    { path: 'myitems', component: MyItemsComponent }
 ];
---
+++ src/app/home/my-items.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-items.component.html',
+  styleUrl: './my-items.component.css'
+})
+export class MyItemsComponent {
+
+}
