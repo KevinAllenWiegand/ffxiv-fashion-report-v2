e890c5173e87b6c5c5664085ea3b254d09d279ca
Kevin Wiegand <KevinAllenWiegand@users.noreply.github.com>
2025-03-06 11:44:34 -0500

    Move away from using IDs
    There's no reason to use the old V1 ID mechanism, since we don't need them anymore with Angular. This simplifies tracking, and the "My Items" section.

--- src/app/services/OwnedItemsService.ts
+++ src/app/services/OwnedItemsService.ts
@@ -1,4 +1,6 @@
 import { Injectable } from "@angular/core";
+import { MasterJsonService } from "./MasterJsonService";
+import { Subscription } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
@@ -7,52 +9,97 @@ export class OwnedItemsService {
     private readonly KEY_V1 = 'ffxiv.fashionreport.items.owned';
     private readonly KEY_V2 = 'ffxiv.fashionreport.v2.items.owned';
 
+    private dataAvailableSubscription: Subscription | undefined;
     private readonly ownedItemsV2 = new Set<string>();
 
-    constructor() {
+    constructor(
+        private readonly masterJsonService: MasterJsonService
+    ) {
         const rawV2 = window.localStorage.getItem(this.KEY_V2);
+        let rawV2Count = 0;
 
         if (rawV2) {
             const items: string[] = JSON.parse(rawV2);
 
+            rawV2Count = items.length;
             items.forEach(item => {
                 this.ownedItemsV2.add(item);
             });
         }
 
-        // If there are no items currently in the new format, check the old format
-        // and import them if we can.
-        if (!rawV2 || !rawV2.length) {
-            const rawV1 = window.localStorage.getItem(this.KEY_V1);
+        // Subscribe to the master json event so we can attempt to migrate.
+        if (rawV2Count === 0) {
+            this.dataAvailableSubscription = masterJsonService.onDataAvailable.subscribe(() => {
+                this.migrateV1ToV2();
+            });
+        }
+    }
 
-            if (rawV1) {
-                const items: string[] = JSON.parse(rawV1);
-
-                items.forEach(item => {
-                    // Be sure to trim off the "item" prefis - that was a silly decision from V1.
-                    this.ownedItemsV2.add(this.makeId(item.substring(4)));
-                    this.save();
-                });
-            }
+    ngOnDestroy(): void {
+        if (this.dataAvailableSubscription) {
+            this.dataAvailableSubscription.unsubscribe();
         }
     }
 
     add(name: string) {
-        this.ownedItemsV2.add(this.makeId(name));
+        this.ownedItemsV2.add(name);
         this.save();
     }
 
     remove(name: string) {
-        this.ownedItemsV2.delete(this.makeId(name));
+        this.ownedItemsV2.delete(name);
         this.save();
     }
 
     isOwned(name: string) {
-        return this.ownedItemsV2.has(this.makeId(name));
+        return this.ownedItemsV2.has(name);
+    }
+
+    private migrateV1ToV2() {
+        const rawV1 = window.localStorage.getItem(this.KEY_V1);
+
+        if (rawV1) {
+            const ids: string[] = JSON.parse(rawV1);
+
+            ids.forEach(id => {
+                // Find the item from the V1 ID.
+                // Be sure to trim off the "item" prefix - that was a silly decision from V1.
+                const name = this.findItemNameFromV1Id(id.substring(4));
+
+                if (name) {
+                    this.ownedItemsV2.add(name);
+                }
+            });
+
+            this.save();
+        }
+    }
+
+    private findItemNameFromV1Id(id: string) {
+        let name = '';
+        const slotCount = this.masterJsonService.masterData?.slots.length || 0;
+
+        for (let slotIndex = 0; slotIndex < slotCount; slotIndex++) {
+            const items = this.masterJsonService.masterData?.slots[slotIndex].items;
+
+            if (!items || !items.length) { continue; }
+
+            for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+                const item = items[itemIndex];
+                const itemId = this.makeId(item.name);
+
+                if (id.toLowerCase() === itemId.toLowerCase()) {
+                    name = item.name;
+                    break;
+                }
+            }
+        }
+
+        return name;
     }
 
     private makeId(name: string) {
-        return name.replace(/[\'\"\s/\(\)\.]/gi, '').toLocaleLowerCase();
+        return name.replace(/[\'\"\s/\(\)\.]/gi, '');
     }
 
     private save() {
